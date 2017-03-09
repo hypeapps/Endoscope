5f9d8f744ed850586fc1a91c0d0e3559c7bf234b
PrzemekEnterprise <pszem.szym@gmail.com>
2017-03-09 17:22:09 +0100

    Added next, done, skip buttons handling

--- app/src/main/java/pl/hypeapp/endoscope/ui/listener/OnDotPageChangeListener.java
+++ app/src/main/java/pl/hypeapp/endoscope/ui/listener/OnDotPageChangeListener.java
@@ -1,7 +1,9 @@
 package pl.hypeapp.endoscope.ui.listener;
 
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +12,20 @@ import pl.hypeapp.endoscope.R;
 
 public class OnDotPageChangeListener implements ViewPager.OnPageChangeListener {
     private List<ImageView> dots = new ArrayList<>();
+    private ImageView nextPageButton;
+    private TextView skipButton;
+    private TextView doneButton;
 
     public OnDotPageChangeListener(List<ImageView> dots) {
         this.dots = dots;
+    }
+
+    public OnDotPageChangeListener(List<ImageView> dots,
+                                   ImageView nextPageButton, TextView skipButton, TextView doneButton) {
+        this(dots);
+        this.nextPageButton = nextPageButton;
+        this.skipButton = skipButton;
+        this.doneButton = doneButton;
     }
 
     @Override
@@ -23,6 +36,8 @@ public class OnDotPageChangeListener implements ViewPager.OnPageChangeListener {
     @Override
     public void onPageSelected(int position) {
         changeDotFocus(position);
+        viewDoneButton(position);
+        viewSkipButton(position);
     }
 
     @Override
@@ -47,5 +62,27 @@ public class OnDotPageChangeListener implements ViewPager.OnPageChangeListener {
 
     private void setInactiveDot(ImageView dot) {
         dot.setImageResource(R.drawable.circle_shape_inactive);
+    }
+
+    private void viewDoneButton(int position) {
+        if ((nextPageButton != null) && (doneButton != null)) {
+            if (position < 3) {
+                nextPageButton.setVisibility(View.VISIBLE);
+                doneButton.setVisibility(View.GONE);
+            } else {
+                nextPageButton.setVisibility(View.GONE);
+                doneButton.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void viewSkipButton(int position) {
+        if (skipButton != null) {
+            if (position > 0) {
+                skipButton.setVisibility(View.GONE);
+            } else {
+                skipButton.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
